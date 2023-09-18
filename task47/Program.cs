b483bf4206b7e1ed5b7c98d7b52647876080e1e6
Yulia Balakina <saika.tocyka@g>
2023-09-18 22:01:24 +0600

    First commit

---
+++ task47/Program.cs
@@ -0,0 +1,36 @@
+﻿double[,] GetRandom2DArray(int rowNumber, int colNumber, int maxValue, int minValue)
+{
+    double[,] result = new double[rowNumber,colNumber];
+    for(int i=0; i< rowNumber;i++)
+    {
+         for(int j=0; j< colNumber;j++)
+         {
+          result[i,j]= new Random().NextDouble()*(maxValue-minValue)+minValue;
+         }
+    }
+    return result;
+}
+
+void Print2DArray(double[,] arrayToPrint)
+{
+    System.Console.Write($"[ ]\t");
+    for(int i=0; i< arrayToPrint.GetLength(1);i++)
+    {
+        Console.Write($"[{i}]\t");
+    }
+    System.Console.WriteLine();
+   for(int i=0; i< arrayToPrint.GetLength(0);i++)
+        {
+            Console.Write($"[{i}]\t");
+             
+            for(int j =0; j<arrayToPrint.GetLength(1);j++)
+            {
+                System.Console.Write(Math.Round(arrayToPrint[i,j],3)+ "\t");
+            } 
+             System.Console.WriteLine();
+             System.Console.WriteLine();
+        } 
+}
+
+double[,] randomArray = GetRandom2DArray(5,5,-10,10);
+Print2DArray(randomArray);
